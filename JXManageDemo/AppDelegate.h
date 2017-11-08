350db4272b1e3f843a7e44e5177cac3ad40cddf8
崔洁祥 <610469644@qq.com>
2017-11-08 10:35:45 +0800

    Initial Commit

---
+++ JXManageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXManageDemo
+//
+//  Created by tet-cjx on 2017/11/8.
+//  Copyright © 2017年 hyd-cjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXManageDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JXManageDemo
+//
+//  Created by tet-cjx on 2017/11/8.
+//  Copyright © 2017年 hyd-cjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
